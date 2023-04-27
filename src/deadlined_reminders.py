cdf33379aa8b3e8a83065d84e35ac2439fd1ee94
Andrew Price <andrew.price1@landregistry.gov.uk>
2023-04-27 12:26:11 +0100

    Update deadlined_reminders.py

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta, abstractmethod, ABC
 from collections.abc import Iterable
+from dateutil.parser import parse
 
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
@@ -12,4 +13,10 @@ class DeadlinedReminder(Iterable, ABC):
     @abstractmethod
     def is_due(self):
         pass
-    
+
+
+class DateReminder(DeadlinedReminder):
+    def __init__(self, text, date):
+        self.date = parse(date, dayfirst=True)
+        self.text = text
+        
